c85124167dd8716a343431344819899d41f75ebb
Sherif Mowafy <sherifmwafy@gmail.com>
2018-01-09 11:00:21 +0200

    Adding product default constructor

--- Boot-Web-MVC-103/src/main/java/com/sherif/bean/Product.java
+++ Boot-Web-MVC-103/src/main/java/com/sherif/bean/Product.java
@@ -2,8 +2,15 @@ package com.sherif.bean;
 
 public class Product {
 	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	public Product() {}
+	
 	private String name;
 	private double price;
+	
 	public String getName() {
 		return name;
 	}
@@ -16,9 +23,6 @@ public class Product {
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	public Product(String name, double price) {
-		this.name = name;
-		this.price = price;
-	}
+	
 	
 }
